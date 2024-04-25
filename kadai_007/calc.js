e00db52d7d4387af7d23a430360fc0a9bb56ca62
koutakasay <165891042+koutakasay@users.noreply.github.com>
2024-04-25 15:00:32 +0900

    calc.jsを変更

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,9 +1,7 @@
 let num = Math.floor(Math.random() * 16);
 console.log(num);
 
-if (num == 0) {
-    console.log(num);
-} else if (num % 3 == 0 && num % 5 == 0) {
+if (num % 3 == 0 && num % 5 == 0) {
     console.log("3と5の倍数です");
 } else if (num % 3 == 0) {
     console.log("3の倍数です");
